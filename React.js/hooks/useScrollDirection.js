699a0a6589cd84a79afb637b0afc2a0ca99d7969
jibolashittubolu <54463012+jibolashittubolu@users.noreply.github.com>
2024-08-09 00:48:05 +0100

    Create useScrollDirection.js

---
+++ React.js/hooks/useScrollDirection.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+
+function useScrollDirection() {
+    const [scrollDirection, setScrollDirection] = useState(null);
+
+    useEffect(() => {
+        const minimumDistance = 10;
+        let lastScrollY = window.pageYOffset;
+        // function to run on scroll
+        const updateScrollDirection = () => {
+            const scrollY = window.pageYOffset;
+            const direction = scrollY > lastScrollY ? "down" : "up";
+            if (direction !== scrollDirection && (scrollY - lastScrollY > minimumDistance || scrollY - lastScrollY < -minimumDistance)) {
+              setScrollDirection(direction);
+            }
+            lastScrollY = scrollY > 0 ? scrollY : 0;
+        };
+        window.addEventListener("scroll", updateScrollDirection); // add event listener
+        return () => {
+            window.removeEventListener("scroll", updateScrollDirection); // clean up
+        }
+    }, [scrollDirection]); // run when scroll direction changes
+
+    return scrollDirection;
+};
